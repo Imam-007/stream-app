60628e688b3be89bef1e27df165c05ab5e523745
imam-007 <imimam8409@gmail.com>
2025-04-23 22:09:56 +0530

    added frontend code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import VideoUpload from "./components/VideoUpload";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,7 +10,10 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-2xl">Welcome to Stream svc application:</h1>
+        <h1 className="font-extrabold text-gray-400">
+          Welcome to Stream svc application
+        </h1>
+        <VideoUpload />
       </div>
     </>
   );
