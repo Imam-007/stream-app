238e8a63f458ffc4f542b70735ebd019491e7d06
imam-007 <imimam8409@gmail.com>
2025-04-28 22:53:34 +0530

    added code send request to play video in the given range

--- src/main/java/com/stream/app/controllers/VideoController.java
+++ src/main/java/com/stream/app/controllers/VideoController.java
@@ -4,13 +4,20 @@ import com.stream.app.entities.Video;
 import com.stream.app.io.CreateResponse;
 import com.stream.app.services.VideoService;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.UUID;
 
@@ -72,7 +79,75 @@ public class VideoController {
                  .body(resource);
     }
 
+    @GetMapping("/videos")
     public List<Video> getAllVideos(){
         return videoService.getAllVideos();
+    }
+
+    @GetMapping("/stream/range/{videoId}")
+    public ResponseEntity<Resource> streamVideoRange(
+            @PathVariable String videoId,
+            @RequestHeader(value = "Range", required = false) String range
+    ){
+        System.out.println(range);
+
+        Video video = videoService.getVideoById(videoId);
+        Path path = Paths.get(video.getFilePath());
+
+        Resource resource = new FileSystemResource(path);
+        String contentType = video.getContentType();
+
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        Long fileLength = path.toFile().length();
+
+        if(range == null) {
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .body(resource);
+        }
+
+        Long startRange, endRange;
+
+        String[] ranges = range.replace("bytes=", "").split("-");
+        startRange = Long.parseLong(ranges[0]);
+
+        if (ranges.length > 1) {
+            endRange = Long.parseLong(ranges[1]);
+        } else {
+            endRange = fileLength - 1;
+        }
+
+        if (endRange > fileLength - 1){
+            endRange = fileLength - 1;
+        }
+
+        InputStream inputStream;
+
+        try {
+            inputStream = Files.newInputStream(path);
+            inputStream.skip(startRange);
+        } catch (IOException e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .build();
+        }
+
+        Long contentLength = endRange - startRange + 1;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Content-Range", "bytes " + startRange + "-" + endRange + "/" + fileLength);
+        httpHeaders.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        httpHeaders.add("Pragma", "no-cache");
+        httpHeaders.add("Expires", "0");
+        httpHeaders.add("X-Content-Type-Options", "nosniff");
+        httpHeaders.setContentLength(contentLength);
+
+        return ResponseEntity
+                .status(HttpStatus.PARTIAL_CONTENT)
+                .headers(httpHeaders)
+                .contentType(MediaType.parseMediaType(contentType))
+                .body(new InputStreamResource(inputStream));
     }
 }
